0a78c887c8cda3d243eaa57c63c26686ff97f172
erhanask <erhanasik8@gmail.com>
2022-08-02 10:29:21 +0300

    More code improvements.

--- resources/js/slider.js
+++ resources/js/slider.js
@@ -1,19 +1,12 @@
 <!-- Initialize Swiper -->
-let paginationImages = [];
-
-$('.swiper-paginate-images').each(function(i, obj) {
-    paginationImages.push($(obj).attr('data-img_src'));
-});
-
-paginationImages.push('{{adv.getMediumPhotoUrl(image)}}');
-
+let paginationImages = $('.swiper-paginate-images').map((i, obj) => $(obj).attr('data-img_src'));
 
 var swiper = new Swiper(".mySwiper", {
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
         renderBullet: function (index, className) {
-            return '<div class="' + className + '">' + '<img class="swiper-border" src=' + paginationImages[index] + ' >' + "</div>";
+            return `<div class="' + className + '"><img class="swiper-border" src="${paginationImages[index]}" ></div>`;
         },
     },
 });
